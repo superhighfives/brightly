ebbb87a831b6f2009c807fa01a09ea70f23ec2ae
Charlie Gleason <cgleason@salesforce.com>
2023-02-06 14:56:53 +0000

    Updated padding, fixed title

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,7 @@
 export default function Layout({ children }) {
-  return <div className="max-w-xl p-2 grid gap-16 py-64">{children}</div>
+  return (
+    <div className="max-w-xl px-4 sm:px-2 grid gap-16 py-[25vh] sm:py-64">
+      {children}
+    </div>
+  )
 }
--- src/components/StreamingService.jsx
+++ src/components/StreamingService.jsx
@@ -1,13 +1,13 @@
 import Title from '~/components/Title'
 
-export default function StreamingService({ href, Icon, name }) {
+export default function StreamingService({ href, Icon, title }) {
   return (
     <a
       className="grid gap-2 pr-1 outline-yellow-500 group min-w-0 p-2 pt-3"
       href={href}
     >
       <Icon className="ml-2 w-8 h-8" />
-      <Title size="xs">{name}</Title>
+      <Title size="xs">{title}</Title>
     </a>
   )
 }
