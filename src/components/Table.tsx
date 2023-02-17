247c17190e66ee14672ef0f1dd77159b5ea7ca63
Charlie Gleason <hi@charliegleason.com>
2023-02-17 22:37:14 +0000

    Got previews going

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,6 @@
 import type { ReactNode } from 'react'
-import { useState, useEffect } from 'react'
 import type { IconType } from 'react-icons'
 import Title from '~/components/Title'
-import { getLinkPreview } from 'link-preview-js'
 
 export default function Table({
   children,
@@ -65,57 +63,4 @@ export function Row({
       </dd>
     </a>
   )
-}
-
-export function Article({
-  id,
-  title,
-  href,
-}: {
-  id: string
-  title: string
-  href: string
-}) {
-  const [preview, setPreview] = useState(null)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(href)
-        if (res.status === 200) {
-          const preview = await getLinkPreview(href)
-          setPreview(preview)
-        }
-      } catch (e) {
-        console.warn(e)
-      }
-    }
-
-    fetchData().catch(console.warn)
-  }, [href])
-
-  return preview ? (
-    <a
-      key={id}
-      href={href}
-      className="py-2 group flex items-center gap-3 outline-yellow-500"
-    >
-      <div className="w-4 h-4 relative ml-2 bg-black opacity-75 mix-blend-screen">
-        <img
-          className="absolute inset-0 grayscale"
-          src={preview?.favicons[0]}
-          alt=""
-        />
-        <div className="w-full h-full absolute bg-yellow-400 mix-blend-multiply"></div>
-      </div>
-      <dt className="truncate font-bold text-xs justify-self-start max-w-full flex">
-        <Title size="xs">{title}</Title>
-      </dt>
-      <dd
-        className={`flex flex-1 font-mono font-semibold text-slate-500 dark:text-yellow-600 text-2xs truncate gap-8 pl-1`}
-      >
-        <span className="truncate max-w-full">{preview?.title}</span>
-      </dd>
-    </a>
-  ) : null
 }
--- src/sections/Publications.tsx
+++ src/sections/Publications.tsx
@@ -1,7 +1,8 @@
 import Heading from '~/components/Heading'
 import Section from '~/components/Section'
 import Group from '~/components/Group'
-import Table, { Article } from '~/components/Table'
+import Table from '~/components/Table'
+import { Article } from '~/components/Article'
 import { ReactComponent as NMELogo } from '~/assets/logo-nme.svg'
 
 export default function Publications() {
@@ -23,20 +24,61 @@ export default function Publications() {
           </div>
         </blockquote>
       </Group>
-
       <Group size="sm">
         <Heading size="sm">Publications and Coverage</Heading>
         <Table>
           <Article
-            id="rugby"
-            title="Rugby"
-            href="https://rugby.wearebrightly.com/"
+            type="Feature"
+            publication="Zealous"
+            title="Making Sad Songs You Can Dance To"
+            href="https://zealous.co/about/archive-2/features-archive/charlie-gleason-electronic-folk-music/"
           />
           <Article
-            id="another-rugby"
-            title="IWNLYG"
-            href="https://iwillneverletyougo.wearebrightly.com/"
+            type="Premiere"
+            publication="Tone Deaf"
+            title="Brightly Unveils Stunning New LP"
+            href="https://tonedeaf.thebrag.com/premiere-brightly-unveils-stunning-new-lp-one-for-sorrow-two-for-joy/"
           />
+          <Article
+            type="Feature"
+            publication="Tone Deaf"
+            title="The 7 Local Records You Should Be Listening To This Week"
+            href="https://tonedeaf.thebrag.com/the-7-local-records-you-should-be-listening-to-this-week-7/"
+          />
+          <Article
+            type="Premiere"
+            publication="Happy Mag"
+            title="Feelin’ dirty?"
+            href="https://happymag.tv/feelin-dirty-brightly-writes-about-sex-death-privilege-and-ball-sports-on-rugby/"
+          />
+          <Article
+            type="Review"
+            publication="AAA Backstage"
+            title="One For Sorrow, Two For Joy"
+            href="https://aaabackstage.com/album-review-brightly-one-for-sorrow-two-for-joy/"
+          />
+
+          <Article
+            type="Review"
+            publication="Renowned For Sound"
+            title="One For Sorrow, Two For Joy"
+            href="https://renownedforsound.com/album-review-brightly-one-for-sorrow-two-for-joy/"
+          />
+          <Article
+            type="Review"
+            publication="Pop Rinse Repeat"
+            title="One For Sorrow, Two For Joy"
+            href="http://www.poprinserepeat.com/2016/06/meet-brightly.html?m=1"
+          />
+
+          {/*
+          <Article
+            type=""
+            publication=""
+            title=""
+            href=""
+          />
+          */}
         </Table>
       </Group>
     </Section>
