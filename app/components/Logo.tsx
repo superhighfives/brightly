ea503f58f2d3aa28e09d487e1b7c5d47fc4df722
Charlie Gleason <hi@charliegleason.com>
2023-02-18 23:47:50 +0000

    Fixed logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link'
 
 export default function Logo() {
   return (
-    <Link href="/">
-      <h1 className="text-4xl sm:text-6xl font-light uppercase tracking-wide pl-2 transition-colors hover:text-slate-900 dark:hover:text-white ">
-        Brightly
-      </h1>
+    <Link
+      href="/"
+      className="outline-yellow-500 text-4xl sm:text-6xl font-light uppercase tracking-wide transition-colors hover:text-slate-900 dark:hover:text-yellow-300"
+    >
+      <h1>Brightly</h1>
     </Link>
   )
 }
