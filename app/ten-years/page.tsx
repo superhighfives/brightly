6d60fded6f74256cd589268e94bf0af01e0a25b1
Charlie Gleason <hi@charliegleason.com>
2023-03-23 11:42:33 +0000

    Started blog post

--- app/ten-years/page.tsx
+++ app/ten-years/page.tsx
@@ -4,16 +4,20 @@ import Layout from '@/app/components/Layout'
 import Logo from '@/app/components/Logo'
 import Social from '@/app/sections/Social'
 import Content from '@/app/components/Content'
+import Visuals from '@/app/visuals/Visuals'
 import MDX from './content.mdx'
 
-export default function About() {
+export default function TenYears() {
   return (
-    <Layout>
-      <Logo />
-      <Content>
-        <MDX />
-      </Content>
-      <Social />
-    </Layout>
+    <>
+      <Visuals />
+      <Layout>
+        <Logo />
+        <Content>
+          <MDX />
+        </Content>
+        <Social />
+      </Layout>
+    </>
   )
 }
