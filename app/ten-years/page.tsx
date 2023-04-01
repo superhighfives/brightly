277ad846e1a6bb907775b6c8a7b1dd976a62d402
Charlie Gleason <hi@charliegleason.com>
2023-04-01 12:18:49 +0100

    Added link back

--- app/ten-years/page.tsx
+++ app/ten-years/page.tsx
@@ -5,6 +5,9 @@ import Logo from '@/app/components/Logo'
 import Social from '@/app/sections/Social'
 import Content from '@/app/components/Content'
 import Visuals from '@/app/visuals/Visuals'
+import Button from '@/app/components/Button'
+import { FaFastBackward } from 'react-icons/fa'
+
 import MDX from './content.mdx'
 
 export default function TenYears() {
@@ -15,6 +18,11 @@ export default function TenYears() {
         <Logo />
         <Content>
           <MDX />
+          <hr />
+          <div className="not-prose dark:text-yellow-400">
+            <Button href="/" Icon={FaFastBackward} title="Back to home" />
+          </div>
+          <hr />
         </Content>
         <Social />
       </Layout>
