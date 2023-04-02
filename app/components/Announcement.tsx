1949eba315525234371245f7de6950c66c234558
Charlie Gleason <hi@charliegleason.com>
2023-04-02 23:59:50 +0100

    Added announcement

---
+++ app/components/Announcement.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import { FaRegClock } from 'react-icons/fa'
+
+export default function Announcement() {
+  return (
+    <Link
+      href="/ten-years"
+      className="bg-stone-50 hover:bg-stone-100 dark:bg-yellow-400 dark:text-slate-950 hover:dark:bg-yellow-500 transition-colors p-3 shadow-xl flex font-semibold gap-2 text-sm outline-yellow-500 outline-offset-4 rounded-md"
+    >
+      <FaRegClock size="16" className="flex-shrink-0 opacity-40 mt-0.5" />
+      <span>
+        Read about the ten year anniversary of{' '}
+        <strong className="font-bold">Beginnings & Endings</strong>
+      </span>
+    </Link>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Intro from '@/app/sections/Intro'
 import Listen from '@/app/sections/Listen'
 import Projects from '@/app/sections/Projects'
 import Publications from '@/app/sections/Publications'
+import Announcement from './components/Announcement'
 import { fetchData } from '@/lib/api'
 
 import Visuals from './visuals/Visuals'
@@ -21,6 +22,7 @@ export default async function Home() {
       <Layout>
         <Logo />
         <Intro />
+        <Announcement />
         <Listen />
         <Discography />
         <Projects />
