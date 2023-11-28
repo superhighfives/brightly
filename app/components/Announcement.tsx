e98bdf57acf5ddd831cbd4d8482f3e9884cf48ec
Charlie Gleason <hi@charliegleason.com>
2023-11-28 21:10:14 +0000

    Updated announcement

--- app/components/Announcement.tsx
+++ app/components/Announcement.tsx
@@ -18,14 +18,17 @@ export default function Announcement() {
         className="hidden dark:block w-[600px]"
       />
 
-      <div className="absolute z-10 top-1/2 left-1/2 bg-yellow-400 text-black rounded px-8 py-2 -translate-x-1/2 -translate-y-1/2 space-x-4 flex items-center pointer-events-none">
+      <a
+        href="https://lysterfieldlake.com/"
+        className="absolute z-10 top-1/2 left-1/2 bg-yellow-400 text-black rounded px-8 py-2 -translate-x-1/2 -translate-y-1/2 space-x-4 flex items-center hover:bg-black hover:text-white group"
+      >
         <span className="whitespace-nowrap font-display italic text-2xl xs:text-3xl">
           Lysterfield Lake
         </span>
-        <span className="whitespace-nowrap uppercase">
-          Dec<span className="hidden xs:inline">ember</span> 1
+        <span className="whitespace-nowrap uppercase border-b border-yellow-600 group-hover:border-stone-600">
+          Visit<span className="hidden sm:inline"> the project</span>
         </span>
-      </div>
+      </a>
     </div>
   )
 }
